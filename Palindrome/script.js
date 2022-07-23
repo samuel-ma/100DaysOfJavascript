1e99d2fcd4f297b7f4291ad27d83cd99c7e6485e
Samuel Majok <82509653+SamiiE99@users.noreply.github.com>
2022-07-23 19:42:51 +0200

    another commit

---
+++ Palindrome/script.js
@@ -0,0 +1,11 @@
+
+
+const name = document.querySelector(".button");
+const result = document.querySelector(".result");
+
+button.addEventListener("click", palindrome);
+
+function palindrome(){
+  const word = document.querySelector(".input-text").value;
+  
+}
