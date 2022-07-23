b28bda390a26c1e7f94beb06e1992b3fa8d1f016
Samuel Majok <82509653+SamiiE99@users.noreply.github.com>
2022-07-23 19:55:29 +0200

    commit

--- Palindrome/script.js
+++ Palindrome/script.js
@@ -9,6 +9,12 @@ function palindrome(){
   const word = document.querySelector(".input-text").value;
   let len = word.length;
   let start = word.substring(0, Math.floor(len/2)).toLowerCase();
+  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+  let flip = end.split('').reverse().join('');
+  if(start == flip){
+    result.innerHTML = `${word.toUppercase} is a Palindrome`;
+  } else {
+    result.innerHTML = `${word.toUpperCase} is not a Palindrome`;
+  }
 
-  
 }
